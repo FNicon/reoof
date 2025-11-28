2c157450e108aa5652700f302ce0cfee01e3b41c
Finiko Kasula Novenda <mybeddrom@gmail.com>
2025-11-28 21:43:36 +0900

    fix main to require example one by one. fix debug msg to use concat

--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
-local examples = {
-  require("examples.ex1"),
-  require("examples.ex2"),
-  require("examples.ex3")
-}
+local concat = table.concat
+local tostring = tostring
+local sformat = string.format
 
 local ex
 local ei = 1
+local emax = 4
 
 local lw, lh = love.graphics.getDimensions()
 local dw = lw
@@ -19,13 +18,13 @@ end
 
 local function loadEx()
   quitEx()
-  ex = examples[ei]
+  ex = require(concat({"examples.ex", tostring(ei)}))
   ex.load()
 end
 
 local function nextEx()
   ei = ei + 1
-  if (ei > #examples) then
+  if (ei > emax) then
     ei = 1
   end
   loadEx()
@@ -34,20 +33,56 @@ end
 local function prevEx()
   ei = ei - 1
   if (ei < 1) then
-    ei = #examples
+    ei = emax
   end
   loadEx()
 end
 
+local debug_msg = {
+  "fps : ",
+  "",
+  " drawcalls : ",
+  "",
+  " drawcallsbatched : ",
+  "",
+  " canvasswitches : ",
+  "",
+  " texturememory : ",
+  "",
+  " images : ",
+  "",
+  " canvases : ",
+  "",
+  " shaderswitches : ",
+  "",
+  " fonts : ",
+  "",
+  " dt : ",
+  "",
+  " is : ",
+  "",
+}
+
 function love.load(_args)
   love.window.setVSync(0)
   if (ex == nil) then
-    if (#_args >= 1 and (type(_args[1]) == "number") or type(_args[1]) == "string") then
-      ei = _args[1]
+    if (#_args >= 1) then
+      if (_args[1] == "debug") then
+        if ((type(_args[2]) == "number") or type(_args[2]) == "string") then
+          ei = _args[2]
+        end
+      else
+        if ((type(_args[1]) == "number") or type(_args[1]) == "string") then
+          ei = _args[1]
+        end
+      end
     end
     loadEx()
   end
 end
+
+local prev_dt = 0
+local delta_label = ""
 
 function love.update(dt)
   if (ex and ex.update) then
@@ -55,6 +90,8 @@ function love.update(dt)
   else
     love.event.quit( "restart" )
   end
+  delta_label = dt > prev_dt and "slower" or "faster"
+  prev_dt = dt
 end
 
 local stats = {}
@@ -64,18 +101,20 @@ local function debug()
   love.graphics.setColor(1, 0, 0, 1)
   love.graphics.rectangle("fill", 0, lh - dh, dw, dh)
   love.graphics.setColor(1, 1, 1, 1)
-  love.graphics.print(
-    "fps : " .. love.timer.getFPS() ..
-    " drawcalls : " .. stats.drawcalls ..
-    " drawcallsbatched : " .. stats.drawcallsbatched ..
-    " canvasswitches : " .. stats.canvasswitches ..
-    " texturememory : " .. stats.texturememory ..
-    " images : " .. stats.images ..
-    " canvases : " .. stats.canvases ..
-    " shaderswitches : " .. stats.shaderswitches ..
-    " fonts : " .. stats.fonts
-    ,0, lh - dh
-  )
+
+  debug_msg[2] = tostring(love.timer.getFPS())
+  debug_msg[4] = tostring(stats.drawcalls)
+  debug_msg[6] = tostring(stats.drawcallsbatched)
+  debug_msg[8] = tostring(stats.canvasswitches)
+  debug_msg[10] = tostring(stats.texturememory)
+  debug_msg[12] = tostring(stats.images)
+  debug_msg[14] = tostring(stats.canvases)
+  debug_msg[16] = tostring(stats.shaderswitches)
+  debug_msg[18] = tostring(stats.fonts)
+  debug_msg[20] = sformat("%.3f", prev_dt)
+  debug_msg[22] = tostring(delta_label)
+
+  love.graphics.print(concat(debug_msg),0, lh - dh)
   love.graphics.print("< (left arrow) prev", 0, lh / 2)
   love.graphics.print("(right arrow) next >", lw - 120, lh/2)
 end
@@ -106,6 +145,12 @@ function love.keypressed(key, scancode, isrepeat)
         ex.keypressed(key, scancode, isrepeat)
       end
     end
+  end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+  if (ex and ex.mousereleased) then
+    ex.mousereleased(x, y, button, istouch, presses)
   end
 end
 
